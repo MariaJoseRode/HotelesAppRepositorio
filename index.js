bf3980f04f58ea430ef090cbf28e2d96f0c9551c
MJ <maroga06@alumnatflorida.es>
2026-03-30 20:44:24 +0200

    Instalacion de paquetes en VSC

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ console.log("Arrancando el servidor...");
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const cors = require("cors");
 
+app.set("port", process.env.PORT || 8080);
+app.set("json spaces", 2);
 
 
 app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
 
-app.set("port", process.env.PORT || 8080);
+
+
 
 app.use(require("./src/routes/index"));
 
