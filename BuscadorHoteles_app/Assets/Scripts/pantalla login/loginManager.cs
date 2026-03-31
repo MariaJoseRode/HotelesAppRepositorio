750a0d607a34dcb7f5ed1d8890e53519942805e7
clarapascual91 <clpahe@alumnatflorida.es>
2026-03-31 03:48:59 +0200

    crear script

---
+++ BuscadorHoteles_app/Assets/Scripts/pantalla login/loginManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using TMPro;
+
+public class loginManager : MonoBehaviour
+{
+    public class LoginResponse
+    {
+        public bool success;
+        public string mensaje; 
+    }
+
+
+
+}
